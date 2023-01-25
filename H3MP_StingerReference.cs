ba18ccafee432e5368a75dd3845ba6b79905e001
Tommy Soucy <soucy.tommy@gmail.com>
2023-01-24 16:04:58 -0800

    Added stinger reference

---
+++ H3MP_StingerReference.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace H3MP
+{
+    public class H3MP_StingerReference : MonoBehaviour
+    {
+        public H3MP_TrackedItem launcher;
+    }
+}
