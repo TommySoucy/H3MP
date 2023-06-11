b9856dce1a56e9b23bbbc3aa2da0db849167c905
Tommy Soucy <soucy.tommy@gmail.com>
2023-06-11 15:34:04 -0700

    Version update

--- src/Mod.cs
+++ src/Mod.cs
@@ -37,7 +37,7 @@ namespace H3MP
         // BepinEx
         public const string pluginGuid = "VIP.TommySoucy.H3MP";
         public const string pluginName = "H3MP";
-        public const string pluginVersion = "1.6.3";
+        public const string pluginVersion = "1.6.4";
 
         // Assets
         public static JObject config;
