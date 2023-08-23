974609920586dc78bad9f8bb19f16af629c73d9e
Tommy Soucy <soucy.tommy@gmail.com>
2023-08-23 10:28:26 -0700

    Fixed TrackedObjectReference trying to set trackedReference while not fully disconnected

--- src/scripts/TrackedObjectReference.cs
+++ src/scripts/TrackedObjectReference.cs
@@ -10,7 +10,7 @@ namespace H3MP.Scripts
 
         private void OnDestroy()
         {
-            if(refIndex != -1 && Mod.managerObject != null)
+            if(refIndex != -1 && Mod.managerObject != null && TrackedObject.trackedReferences.Length > refIndex)
             {
                 TrackedObject.trackedReferences[refIndex] = null;
                 TrackedObject.trackedReferenceObjects[refIndex] = null;
